5c07889d804276fa691d5c5274d335b10a993969
rrodrigodlima <rdrigo019@gmail.com>
2023-08-10 00:23:11 -0300

    requirement 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,30 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # Invalid key type
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('Trybe', 'A')
+
+    # Invalid message type
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(20, 2)
+
+    # Testando a criptografia com índice ímpar
+    assert encrypt_message("hello", 2) == "oll_eh"
+
+    # Testando a criptografia com índice par
+    assert encrypt_message("hello", 3) == "leh_ol"
+
+    # Testando índice maior que o comprimento da mensagem
+    assert encrypt_message("hello", 10) == "olleh"
+
+    # Testando a criptografia com índice igual ao comprimento da mensagem
+    assert encrypt_message("world", 5) == "dlrow"
+
+    # Testando a criptografia com índice zero (deve inverter a mensagem)
+    assert encrypt_message("python", 0) == "nohtyp"
+
+    # Testando a criptografia com índice 1 (deve inverter a mensagem)
+    assert encrypt_message("example", 1) == "e_elpmax"
