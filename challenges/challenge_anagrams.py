e583c563601a23367043c985ad304b53becb7b3f
rrodrigodlima <rdrigo019@gmail.com>
2023-08-10 01:40:36 -0300

    requirement 4 and 6

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
+def quicksort(string):
+    if len(string) <= 1:
+        return string
+    else:
+        pivot = string[0]
+        left = [x for x in string[1:] if x < pivot]
+        right = [x for x in string[1:] if x >= pivot]
+        return (quicksort(left) + [pivot] + quicksort(right))
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui"""
-    raise NotImplementedError
+    if first_string == '' and second_string == '':
+        return ('', '', False)
+
+    first_sorted = ''.join(quicksort(first_string.lower()))
+    second_sorted = ''.join(quicksort(second_string.lower()))
+    return (first_sorted, second_sorted, first_sorted == second_sorted)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list):
+        return False
+
+    nums_set = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in nums_set:
+            return num
+        nums_set.add(num)
+
+    return False
