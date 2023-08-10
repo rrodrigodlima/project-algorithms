1800cffff701a249801116f8ecd803c35291a216
rrodrigodlima <rdrigo019@gmail.com>
2023-08-09 23:36:54 -0300

    requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    if any(
+        not isinstance(start, int) or not isinstance(end, int)
+        for start, end in permanence_period
+    ):
+        return None
+    count = sum(
+        1 for start, end in permanence_period if start <= target_time <= end
+        )
+    return count
